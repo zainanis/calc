132760c72961b28d9398d0b806b794181c1b084c
zainanis <65032146+zainanis@users.noreply.github.com>
2020-06-26 09:39:31 -0700

    Add files via upload

---
+++ calc/calc.js
@@ -0,0 +1,20 @@
+
+var firstNum
+function printNum(num){
+
+  
+   document.getElementById("screen").value=document.getElementById("screen").value+num
+
+
+}
+
+function Sol(){
+
+    document.getElementById("screen").value=eval(document.getElementById("screen").value)
+
+}
+function Clear(){
+
+    document.getElementById("screen").value=""
+
+}
